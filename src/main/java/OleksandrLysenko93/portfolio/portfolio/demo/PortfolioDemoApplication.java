1dc16cfd4dc49bbab43a4d4ef162845304aae7ec
OleksandrLysenko93 <tarabanovka3@gmail.com>
2020-11-21 11:31:38 +0100

    structure :: initial project structure

---
+++ src/main/java/OleksandrLysenko93/portfolio/portfolio/demo/PortfolioDemoApplication.java
@@ -0,0 +1,13 @@
+package OleksandrLysenko93.portfolio.portfolio.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/OleksandrLysenko93/portfolio/portfolio/demo/PortfolioDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package OleksandrLysenko93.portfolio.portfolio.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
